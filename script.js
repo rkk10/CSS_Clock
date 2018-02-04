d547679ec956c61a98f43a0bc4c48f7542c2fcc7
Ravindra Khandagale <ravindrakk143@gmail.com>
2018-02-05 01:17:51 +0530

    clock done

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ sec_hand = document.getElementById('sec')
 
 function setDate(){
     const now = new Date()
+
     const seconds = now.getSeconds();
     const secondDegrees = ((seconds / 60) * 360) + 90
     sec_hand.style.transform = `rotate(${secondDegrees}deg)`;
-    console.log(seconds)
+   
+    const minuts = now.getMinutes();
+    const minDegrees = ((minuts / 60) * 360) + 90
+    min_hand.style.transform = `rotate(${minDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((minuts / 12) * 360) + 90
+    hour_hand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 setInterval(setDate,1000) 
